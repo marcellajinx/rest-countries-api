623c35ee864d8a409663e88b7ee884945201bfa2
marcellajinx <cellakomunita@gmail.com>
2022-01-09 19:47:07 +0700

    add deploy

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -37674,7 +37674,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61588" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60298" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
